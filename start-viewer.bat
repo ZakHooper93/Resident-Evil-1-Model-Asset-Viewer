0415dbdc20f25a8c9a610b06b3f70328eed66408
Zak Hooper <zakhooper93@gmail.com>
2026-06-01 18:11:53 +0100

    Ignore local start script

--- start-viewer.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-echo Starting RE1 PS1 model viewer...
-echo.
-"C:\Users\zaksh\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe" server.js
-echo.
-echo Server stopped. Press any key to close this window.
-pause >nul
